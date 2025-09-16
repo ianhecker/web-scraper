31e0c74996c19d1f26a704b1a8a334bb085ebf7f
ian hecker <ianhecker.dev@gmail.com>
2025-09-16 15:02:18 -0500

    Feature[job] Add marshal/unmarshal to CSV

--- job/job.go
+++ job/job.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -21,15 +22,7 @@ type Job struct {
 	URL      url.URL
 }
 
-func MakeJob(
-	title,
-	company,
-	salary,
-	dateStr,
-	location string,
-	isRemote bool,
-	urlStr string,
-) (Job, error) {
+func MakeJob(title, company, salary, dateStr, location string, isRemote bool, urlStr string) (Job, error) {
 	date, err := time.Parse("02-01-2006", dateStr)
 	if err != nil {
 		return Job{}, fmt.Errorf("error parsing date: %w", err)
@@ -55,12 +48,7 @@ func MakeJob(
 	return job, nil
 }
 
-func MakeID(
-	title,
-	company string,
-	date time.Time,
-	url url.URL,
-) string {
+func MakeID(title, company string, date time.Time, url url.URL) string {
 	seed := title +
 		"|" + company +
 		"|" + date.Format("02-01-2006") +
@@ -68,6 +56,51 @@ func MakeID(
 	base := strings.ToLower(seed)
 	sum := sha1.Sum([]byte(base))
 	return hex.EncodeToString(sum[:])[:7]
+}
+
+func (j Job) MarshalCSV() []string {
+	return []string{
+		j.ID,
+		j.Title,
+		j.Company,
+		j.Salary,
+		j.Date.Format("02-01-2006"),
+		j.Location,
+		fmt.Sprintf("%t", j.IsRemote),
+		j.URL.String(),
+	}
+}
+
+func (j *Job) UnmarshalCSV(record []string) error {
+	if len(record) != 8 {
+		return fmt.Errorf("invalid csv record length: %s", len(record))
+	}
+
+	j.ID = record[0]
+	j.Title = record[1]
+	j.Company = record[2]
+	j.Salary = record[3]
+	j.Location = record[5]
+
+	date, err := time.Parse("02-01-2006", record[4])
+	if err != nil {
+		return fmt.Errorf("error parsing date: %w", err)
+	}
+	j.Date = date
+
+	isRemote, err := strconv.ParseBool(record[6])
+	if err != nil {
+		return fmt.Errorf("error parsing bool: %w", err)
+	}
+	j.IsRemote = isRemote
+
+	URL, err := url.Parse(record[7])
+	if err != nil {
+		return fmt.Errorf("error parsing URL: %w", err)
+	}
+	j.URL = *URL
+
+	return nil
 }
 
 func (j Job) MarshalJSON() ([]byte, error) {
