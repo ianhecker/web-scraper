f8481f5723e683a537a734cc986fe07fae695fe7
ian hecker <ianhecker.dev@gmail.com>
2025-09-16 11:17:21 -0500

    Feature[main.go] Scrape a page

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/ianhecker/web-scraper/scrape"
+)
+
 func main() {
-	
+	url := "https://gojobs.run"
+
+	body, _, err := scrape.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
