aefa86abd177cda09bb36be3b7274f3fa2f795ce
ian hecker <ianhecker.dev@gmail.com>
2025-09-16 13:15:34 -0500

    Chore[job] Add hash ID

--- job/job.go
+++ job/job.go
@@ -1,13 +1,17 @@
 package job
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"strings"
 	"time"
 )
 
 type Job struct {
+	ID       string
 	Title    string
 	Company  string
 	Salary   string
@@ -15,31 +19,6 @@ type Job struct {
 	Location string
 	IsRemote bool
 	URL      url.URL
-}
-
-func (j Job) MarshalJSON() ([]byte, error) {
-	tmp := struct {
-		Title    string
-		Company  string
-		Salary   string
-		Date     string
-		Location string
-		IsRemote bool
-		URL      string
-	}{
-		Title:    j.Title,
-		Company:  j.Company,
-		Salary:   j.Salary,
-		Date:     j.Date.Format("02-01-2006"),
-		Location: j.Location,
-		IsRemote: j.IsRemote,
-		URL:      j.URL.String(),
-	}
-	bytes, err := json.Marshal(tmp)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling job: %w", err)
-	}
-	return bytes, nil
 }
 
 func MakeJob(
@@ -61,7 +40,10 @@ func MakeJob(
 		return Job{}, fmt.Errorf("error parseing URL: %w", err)
 	}
 
+	ID := MakeID(title, company, date, *URL)
+
 	job := Job{
+		ID:       ID,
 		Title:    title,
 		Company:  company,
 		Salary:   salary,
@@ -71,6 +53,48 @@ func MakeJob(
 		URL:      *URL,
 	}
 	return job, nil
+}
+
+func MakeID(
+	title,
+	company string,
+	date time.Time,
+	url url.URL,
+) string {
+	seed := title +
+		"|" + company +
+		"|" + date.Format("02-01-2006") +
+		"|" + url.String()
+	base := strings.ToLower(seed)
+	sum := sha1.Sum([]byte(base))
+	return hex.EncodeToString(sum[:])[:7]
+}
+
+func (j Job) MarshalJSON() ([]byte, error) {
+	tmp := struct {
+		ID       string
+		Title    string
+		Company  string
+		Salary   string
+		Date     string
+		Location string
+		IsRemote bool
+		URL      string
+	}{
+		ID:       j.ID,
+		Title:    j.Title,
+		Company:  j.Company,
+		Salary:   j.Salary,
+		Date:     j.Date.Format("02-01-2006"),
+		Location: j.Location,
+		IsRemote: j.IsRemote,
+		URL:      j.URL.String(),
+	}
+	bytes, err := json.Marshal(tmp)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling job: %w", err)
+	}
+	return bytes, nil
 }
 
 func MakeJobsFromRawJobs(rawJobs []Raw) ([]Job, error) {
