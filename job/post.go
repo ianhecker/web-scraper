4d9ead77f26f3943f033f6944c32bac5bc02c964
ian hecker <ianhecker.dev@gmail.com>
2025-09-16 12:28:41 -0500

    Chore[job] Add space trimming during post creation

--- job/post.go
+++ job/post.go
@@ -1,21 +1,53 @@
 package job
 
+import "strings"
+
 type Post struct {
-	Title     string
-	Company   string
-	Salary    string
-	Date      string
-	URL       string
-	Locations []string
+	Title    string
+	Company  string
+	Salary   string
+	Date     string
+	Location string
+	IsRemote bool
+	URL      string
 }
 
-func MakePost(title, company, salary, date, url string, location []string) Post {
+func MakePost(
+	title,
+	company,
+	salary,
+	date,
+	location string,
+	isRemote bool,
+	url string,
+) Post {
 	return Post{
-		Title:     title,
-		Company:   company,
-		Salary:    salary,
-		Date:      date,
-		URL:       url,
-		Locations: location,
+		Title:    title,
+		Company:  company,
+		Salary:   salary,
+		Date:     date,
+		Location: location,
+		IsRemote: isRemote,
+		URL:      url,
 	}
+}
+
+func MakePostFromDoc(
+	title,
+	company,
+	salary,
+	date,
+	location string,
+	isRemote bool,
+	url string,
+) Post {
+	return MakePost(
+		strings.TrimSpace(title),
+		strings.TrimSpace(company),
+		strings.TrimSpace(salary),
+		strings.TrimSpace(date),
+		strings.TrimSpace(location),
+		isRemote,
+		strings.TrimSpace(url),
+	)
 }
